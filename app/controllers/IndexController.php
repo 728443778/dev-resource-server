6969e8beec765a4134e92f7c6f7e1bb99ae3d14d
sa <sa@localhost.localdomain>
2017-11-15 23:59:40 +0800

    fix bug

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,15 +2,43 @@
 
 namespace app\controllers;
 
+use app\models\Clients;
 use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\Dispatcher;
 use SebastianBergmann\CodeCoverage\Util;
 use sevenUtils\resources\DevManager\Utils;
 
 class IndexController extends Controller
 {
+    protected $client;
+
+    /**
+     * @param $dispatcher Dispatcher
+     */
+    public function beforeExecuteRoute($dispatcher)
+    {
+        $clienId = $this->request->getPost('access_id');
+        $token = $this->request->getPost('access_token');
+        $accessAt = $this->request->getPost('access_at');
+        $client = Clients::findFirst([
+            'condition' => 'access_id=?0 and status=1',
+            'limit' => 1,
+            'bind' => [$clienId],
+            'cache' => [
+                'key' => 'client-' . $clienId,
+                'lifetime' => 3600
+            ],
+        ]);
+        if (!$client) {
+            $this->responseJson(ERROR_NONE);
+            return false;
+        }
+    }
+
     protected function responseJson($code = ERROR_NONE, $data = [])
     {
         $data['code'] = $code;
+        return $this->response->setJsonContent($data);
     }
 
     public function indexAction()
@@ -27,6 +55,8 @@ class IndexController extends Controller
                 return $this->responseJson(ERROR_OPERATION_FAILED);
         }
     }
+
+
 
     protected function deleteBucket()
     {
